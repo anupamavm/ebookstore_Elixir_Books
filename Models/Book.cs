50e568b90e428283a0931c25e011a806bb6bb79c
anupamavm <63632098+anupamavm@users.noreply.github.com>
2023-03-01 22:28:22 +0530

    two

--- Models/Book.cs
+++ Models/Book.cs
@@ -12,9 +12,7 @@ namespace ebookstore.Models
         public string bookLanguage { get; set; }
         public string bookDescription { get; set; }
 
-        [ForeignKey("Author")]
-        public int AuthorId { get; set; }
-        public virtual Author Author { get; set; }
+       
 
     }
 }
